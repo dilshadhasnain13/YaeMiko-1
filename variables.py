4b0d7b74cf9c9e84c057df80af55f3fbc41d2f63
dilshadhasnain07 <161922960+dilshadhasnain07@users.noreply.github.com>
2024-03-10 00:11:06 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,85 @@
+# https://github.com/Infamous-Hydra/YaeMiko
+# https://github.com/Team-ProjectCodeX
+
+
+class Config(object):
+    # Configuration class for the bot
+
+    # Enable or disable logging
+    LOGGER = True
+
+    # <================================================ REQUIRED ======================================================>
+    # Telegram API configuration
+    API_ID = 24683098  # Get this value from my.telegram.org/apps
+    API_HASH = "e4055cd239464e50e69bd73057c05dd3"
+
+    # Database configuration (PostgreSQL)
+    DATABASE_URL = "postgres://ierjlkr:OG4dxzO67Zret3Zii43Hhvujkg89WVry0n9KsHE@karma.db.elephantsql.com/ierjlkr"
+
+    # Event logs chat ID and message dump chat ID
+    EVENT_LOGS = -1002074034670
+    MESSAGE_DUMP = -1002074034670
+
+    # MongoDB configuration
+    MONGO_DB_URI = "mongodb+srv://t45:t45@cluster0.plfylpo.mongodb.net/?retryWrites=true&w=majority"
+
+    # Support chat and support ID
+    SUPPORT_CHAT = "ANIME_FOREST_07"
+    SUPPORT_ID = -1001629811868
+
+    # Database name
+    DB_NAME = "MikoDB"
+
+    # Bot token
+    TOKEN = "6958105710:AAEAMlWeHd5RYwMvwBrPtBfWC0Xjmh6phOI"  # Get bot token from @BotFather on Telegram
+
+    # Owner's Telegram user ID (Must be an integer)
+    OWNER_ID = 6346273488
+    # <=======================================================================================================>
+
+    # <================================================ OPTIONAL ======================================================>
+    # Optional configuration fields
+
+    # List of groups to blacklist
+    BL_CHATS = []
+
+    # User IDs of sudo users, dev users, support users, tiger users, and whitelist users
+    DRAGONS = []  # Sudo users
+    DEV_USERS = []  # Dev users
+    DEMONS = []  # Support users
+    TIGERS = []  # Tiger users
+    WOLVES = []  # Whitelist users
+
+    # Toggle features
+    ALLOW_CHATS = True
+    ALLOW_EXCL = True
+    DEL_CMDS = True
+    INFOPIC = True
+
+    # Modules to load or exclude
+    LOAD = []
+    NO_LOAD = []
+
+    # Global ban settings
+    STRICT_GBAN = True
+
+    # Temporary download directory
+    TEMP_DOWNLOAD_DIRECTORY = "./"
+    # <=======================================================================================================>
+
+
+# <=======================================================================================================>
+
+
+class Production(Config):
+    # Production configuration (inherits from Config)
+
+    # Enable or disable logging
+    LOGGER = True
+
+
+class Development(Config):
+    # Development configuration (inherits from Config)
+
+    # Enable or disable logging
+    LOGGER = True
